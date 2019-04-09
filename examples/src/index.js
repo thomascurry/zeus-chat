f44fe22051c2c6982d993f4008424b899ba94570
thomascurry <thomas.curry@hotmail.com>
2019-04-09 15:57:45 +0100

    Change .env injection to be at runtime

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Zeus from '../../src/Chatbot';
+import React from "react";
+import { render } from "react-dom";
+import Zeus from "../../src/Chatbot";
 
 function zeusChat(domOptions) {
-	const envOptions = {
-		googleProjectId: process.env.GOOGLE_PROJECT_ID,
-		dialogFlowSessionId: process.env.DF_SESSION_ID,
-		googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-		googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY
-	};
+  const envOptions = {
+    googleProjectId: process.env.GOOGLE_PROJECT_ID,
+    dialogFlowSessionId: process.env.DF_SESSION_ID,
+    googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
+    googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY
+  };
 
-	const options = Object.assign(envOptions, domOptions);
+  const options = Object.assign(envOptions, domOptions);
 
-	if (!options.element) {
-		throw new Error('dom element to mount chatbot view has not been defined');
-	}
-	if (!options.googleProjectId) {
-		throw new Error('please set the GOOGLE_PROJECT_ID environment variable');
-	}
-	if (!options.dialogFlowSessionId) {
-		throw new Error('please set the DF_SESSION_ID environment variable');
-	}
-	if (!options.googleClientEmail) {
-		throw new Error('please set the GOOGLE_CLIENT_EMAIL environment variable');
-	}
-	if (!options.googlePrivateKey) {
-		throw new Error('please set the GOOGLE_PRIVATE_KEY environment variable');
-	}
+  if (!options.element) {
+    throw new Error("dom element to mount chat view has not been defined");
+  }
+  if (!options.googleProjectId) {
+    throw new Error("please set the GOOGLE_PROJECT_ID environment variable");
+  }
+  if (!options.dialogFlowSessionId) {
+    throw new Error("please set the DF_SESSION_ID environment variable");
+  }
+  if (!options.googleClientEmail) {
+    throw new Error("please set the GOOGLE_CLIENT_EMAIL environment variable");
+  }
+  if (!options.googlePrivateKey) {
+    throw new Error("please set the GOOGLE_PRIVATE_KEY environment variable");
+  }
 
-	render(<Zeus {...options} />, options.element);
+  render(<Zeus {...options} />, options.element);
 }
 
 export default zeusChat;
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,53 @@
-const path = require('path');
+let dotenv = require("dotenv").config({ path: __dirname + "/.env" });
 
-const Dotenv = require('dotenv-webpack');
-const dotEnv = new Dotenv();
+const path = require("path");
+const webpack = require("webpack");
+// const Dotenv = require('dotenv-webpack');
+// const dotEnv = new Dotenv();
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
-	template: path.join(__dirname, 'examples/src/index.html'),
-	filename: './index.html'
+  template: path.join(__dirname, "examples/src/index.html"),
+  filename: "./index.html"
 });
 
 module.exports = {
-	entry: path.join(__dirname, 'examples/src/index.js'),
-	output: {
-		path: path.join(__dirname, 'examples/dist'),
-		filename: 'zeus-chat.js',
-		library: 'zeusChat',
-		libraryExport: 'default',
-		libraryTarget: 'umd'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				use: 'babel-loader',
-				exclude: /node_modules/
-			},
-			{
-				test: /\.css$/,
-				use: [ 'style-loader', 'css-loader' ]
-			}
-		]
-	},
-	node: {
-		fs: 'empty',
-		net: 'empty',
-		tls: 'empty'
-	},
-	plugins: [ htmlWebpackPlugin, dotEnv ],
-	resolve: {
-		extensions: [ '.js', '.jsx' ]
-	},
-	devServer: {
-		port: 3001
-	}
+  entry: path.join(__dirname, "examples/src/index.js"),
+  output: {
+    path: path.join(__dirname, "examples/dist"),
+    filename: "zeus-chat.js",
+    library: "zeusChat",
+    libraryExport: "default",
+    libraryTarget: "umd"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  node: {
+    fs: "empty",
+    net: "empty",
+    tls: "empty"
+  },
+  plugins: [
+    htmlWebpackPlugin,
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(dotenv.parsed)
+    })
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  devServer: {
+    port: 3001
+  }
 };
