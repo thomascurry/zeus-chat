fc80343532b174f4ea933630e11ebaedea0ba7fe
thomascurry <thomas.curry@hotmail.com>
2019-04-12 14:37:09 +0100

    More moving styling into styles.css

--- src/Message.js
+++ src/Message.js
@@ -5,67 +5,21 @@ const Message = props => (
   <div>
     <div className="message">
       {props.speaks === 'bot' && (
-        <div>
-          <div>
-            <span className="text">{`${props.speaks} ${props.time}`}</span>
-            <div
-              style={{
-                width: 350,
-                height: 'auto',
-                display: 'block',
-                background: '#f0f0f0',
-                borderRadius: 4,
-                position: 'relative',
-                margin: 'auto',
-              }}
-            >
-              <div
-                style={{
-                  padding: '8px 55px 8px 14px',
-                }}
-                className="col 10"
-              >
-                <span
-                  style={{
-                    fontSize: 15,
-                    margin: 0,
-                    color: '#2b2b2b',
-                  }}
-                >
-                  {props.text}
-                </span>
-              </div>
+        <React.Fragment>
+          <span className="bot-speaks">{`${props.speaks} ${props.time}`}</span>
+          <div className="text-container">
+            <div className="text-background">
+              <span className="text" l>
+                {props.text}
+              </span>
             </div>
           </div>
-        </div>
+        </React.Fragment>
       )}
       {props.speaks === 'me' && (
-        <div>
-          <div
-            style={{
-              fontWeight: 400,
-              width: 325,
-              height: 'auto',
-              display: 'block',
-              borderRadius: 4,
-              position: 'relative',
-              margin: 'auto',
-            }}
-          >
-            <span
-              style={{
-                fontWeight: 600,
-                fontSize: 14,
-                position: 'absolute',
-                bottom: 5,
-                right: 0,
-                color: '#999',
-                textTransform: 'capitalize',
-              }}
-            >
-              {`${props.speaks} ${props.time}`}
-            </span>
-          </div>
+        <React.Fragment>
+          <span className="user-speaks">{`${props.speaks} ${props.time}`}</span>
+
           <div
             style={{
               width: 350,
@@ -107,7 +61,7 @@ const Message = props => (
               </span>
             </div>
           </div>
-        </div>
+        </React.Fragment>
       )}
     </div>
   </div>
